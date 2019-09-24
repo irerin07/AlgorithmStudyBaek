b75eac86521bf81b86b397ffde25e75f4c477dbd
irerin07 <counterchord89@gmail.com>
2019-09-24 16:34:10 +0900

    프로그래머스 완주하지 못한 선수

---
+++ src/programmers42576.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class programmers42576 {
+    public static void main(String[] args) {
+        String[] praticipant = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String [] completion = {"josipa", "filipa", "marina", "nikola"};
+        System.out.println(solution(praticipant, completion));
+    }
+
+    public static String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for(int i = 0; i < completion.length; i++){
+            if(!completion[i].equals(participant[i])){
+                return participant[i];
+            }else{
+                answer = participant[completion.length];
+            }
+        }
+        return answer;
+    }
+
+}
