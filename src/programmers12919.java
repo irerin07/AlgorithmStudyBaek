b09cd49f411d72626acb9f2ec534fdebdb877697
irerin07 <counterchord89@gmail.com>
2020-04-03 18:01:56 +0900

    서울에서 김서방 찾기

---
+++ src/programmers12919.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class programmers12919 {
+    public static void main(String[] args) {
+        String[] seoul = {"Jane", "Kim"};
+        System.out.println(solution(seoul));
+    }
+
+    private static String solution(String[] seoul) {
+        String answer = "";
+        List<String> list = Arrays.asList(seoul);
+        answer = "김서방은 " +list.indexOf("Kim")+ "에 있다";
+        return answer;
+    }
+}
