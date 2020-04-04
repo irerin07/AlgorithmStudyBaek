8284201a31d27e40dad555b7d44e16721d58a4c0
irerin07 <counterchord89@gmail.com>
2020-04-04 23:22:27 +0900

    수박수박수박수박수박수?

---
+++ src/programmers12922.java
@@ -0,0 +1,19 @@
+public class programmers12922 {
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(solution(n));
+    }
+
+    private static String solution(int n) {
+        String answer = "";
+
+        for(int i = 1; i <= n; i++){
+            if(i%2 != 0){
+                answer += "수";
+            }else{
+                answer+="박";
+            }
+        }
+        return answer;
+    }
+}
