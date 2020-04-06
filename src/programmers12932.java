6366f61537086d319c31aa0d177c51cbe8bb453d
irerin07 <counterchord89@gmail.com>
2020-04-06 16:33:04 +0900

    자연수 뒤집어 배열로 만들기

--- src/programmers12932.java
+++ src/programmers12932.java
@@ -1,13 +1,16 @@
 public class programmers12932 {
     public static void main(String[] args) {
-        long n = 12345;
+        long n = 2147483647;
         int[] n1 = solution(n);
     }
 
     private static int[] solution(long n) {
-        int[] answer = {};
-        while(n > 0){
-
+        int[] answer = new int[String.valueOf(n).length()];
+        int index = 0;
+        while (n > 0) {
+            n = n % 10;
+            n /= 10;
+            answer[index++] = (int) n;
         }
         return answer;
     }
---
+++ src/programmers12933.java
@@ -0,0 +1,5 @@
+public class programmers12933 {
+    public static void main(String[] args) {
+
+    }
+}
