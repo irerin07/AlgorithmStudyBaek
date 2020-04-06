69f7730fb8bfb7d6dff4b3185578ae539583dffc
irerin07 <counterchord89@gmail.com>
2020-04-06 16:41:42 +0900

    정수 내림차순으로 배치하기

--- src/programmers12933.java
+++ src/programmers12933.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class programmers12933 {
     public static void main(String[] args) {
+        long n = 118372;
+        System.out.println(solution(n));
+    }
 
+    private static long solution(long n) {
+        long answer = 0;
+        String[] s = String.valueOf(n).split("");
+        String temp = "";
+
+        Arrays.sort(s, Collections.reverseOrder());
+        for(String s1: s){
+            temp += s1;
+        }
+        answer = Long.parseLong(temp);
+        return answer;
     }
 }
