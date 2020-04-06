6284fadce88c0c849fcecbd373f428876bda3482
irerin07 <counterchord89@gmail.com>
2020-04-06 23:18:26 +0900

    짝수와 홀수

---
+++ src/programmers12937.java
@@ -0,0 +1,14 @@
+public class programmers12937 {
+    public static void main(String[] args) {
+        int num = 3;
+        System.out.println(solution(num));
+    }
+
+    private static String solution(int num) {
+        String answer = "";
+        // answer = (num%2 == 0) ? "Even":"Odd";
+
+        return (num%2 == 0) ? "Even":"Odd";
+
+    }
+}
