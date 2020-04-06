ec3ba5fa3b50e7eb79453d8f7630a89f6550d0f8
irerin07 <counterchord89@gmail.com>
2020-04-06 18:04:35 +0900

    제일 작은 수 제거라기

---
+++ src/programmers12935.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class programmers12935 {
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 2, 1};
+        int[] sol = solution(arr);
+        for (int i : sol) {
+            System.out.println(i);
+        }
+    }
+
+    private static int[] solution(int[] arr) {
+//        if (arr.length <= 1) return new int[]{ -1 };
+//        int min = Arrays.stream(arr).min().getAsInt();
+//        return Arrays.stream(arr).filter(i -> i != min).toArray();
+        int[] answer = {};
+        int min = arr[0];
+        if (arr.length == 1) {
+            return new int[]{-1};
+        }
+        for (int i = 0; i < arr.length; i++) {
+            min = Math.min(arr[i], min);
+        }
+        answer = new int[arr.length - 1];
+
+        int j = 0;
+        for (int i = 0; i < answer.length; i++) {
+            if (arr[j] == min) {
+                j++;
+                i--;
+                continue;
+            }
+            answer[i] = arr[j];
+            j++;
+        }
+        return answer;
+
+    }
+}
