52bee73ea333a6599ed3f44eb859e37bd35bade1
irerin07 <counterchord89@gmail.com>
2020-04-07 00:55:33 +0900

    평균 구하기

---
+++ src/programmers12944.java
@@ -0,0 +1,19 @@
+public class programmers12944 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        System.out.println(solution(arr));
+    }
+
+    private static double solution(int[] arr) {
+        double answer = 0;
+        for(int s: arr){
+            answer = answer + s;
+        }
+//        if(answer > 0){
+//            answer = answer/arr.length;
+//        }else{
+//            answer = 0;
+//        }
+        return answer/arr.length;
+    }
+}
