f6ba8a1e25a7f5fe927610d73a67f06cf14af874
irerin07 <counterchord89@gmail.com>
2020-04-07 14:01:22 +0900

    핸드폰 번호 가리기

---
+++ src/programmers12948.java
@@ -0,0 +1,31 @@
+public class programmers12948 {
+    public static void main(String[] args) {
+        String phone_number = "01033334444";
+        System.out.println(solution(phone_number));
+    }
+
+    private static String solution(String phone_number) {
+//        char[] ch = phone_number.toCharArray();
+//        for(int i = 0; i < ch.length - 4; i ++){
+//            ch[i] = '*';
+//        }
+//        return String.valueOf(ch);
+        String answer = "";
+        StringBuffer sb = new StringBuffer();
+
+        if(phone_number.length() == 4){
+            return phone_number;
+        }
+        for(int i = 0; i < phone_number.length(); i++){
+            if(i < phone_number.length() - 4) {
+                sb.append("*");
+            }else{
+                sb.append(phone_number.charAt(i));
+            }
+        }
+        System.out.println(sb);
+
+
+        return sb.toString();
+    }
+}
