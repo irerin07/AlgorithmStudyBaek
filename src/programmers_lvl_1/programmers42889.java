934062acefa5bd81815ad7837e067cbabaf8ee16
irerin07 <counterchord89@gmail.com>
2020-04-08 13:16:46 +0900

    실패율

--- src/programmers_lvl_1/programmers42889.java
+++ src/programmers_lvl_1/programmers42889.java
@@ -14,47 +14,87 @@ public class programmers42889 {
         int[] answer = solution(N, stages);
     }
 
-    private static int[] solution(int n, int[] stages) {
-        int[] answer = new int[n];
-        int[] player_failed = new int[n+1];
-        double[] stage_failure_rate = new double[n+1];
-        int total_player = stages.length;
-        for(int i = 0; i < stages.length; i++){
-            if(stages[i] <= n) {
-                player_failed[stages[i]] += 1;
-            }else{
-                continue;
+    private static int[] solution(int N, int[] stages) {
+        int remain[] = new int[N+1];
+        int pass[] = new int[N];
+        for(int i=0; i<stages.length;i++) {
+            remain[stages[i]-1]++;
+            for(int j =0; j<stages[i]-1; j++) {
+                pass[j]++;
             }
         }
-        for(int s: player_failed){
-            System.out.println(s);
-        }
-        for(int i = 1; i < stage_failure_rate.length; i++){
-            System.out.println("player_failed[i]: " + player_failed[i]);
-            if(player_failed[i] > 0) {
-                stage_failure_rate[i] = (double) player_failed[i] / total_player;
-                total_player -= player_failed[i];
-            }else{
-                stage_failure_rate[i] = 0;
+        double[] fail = new double[N];
+        for(int i= 0; i<N; i++) {
+            if(pass[i]==0&&remain[i]==0) {
+                fail[i]=0;
             }
+            else
+                fail[i] = (double)remain[i]/(double)pass[i];
         }
 
-        List stage_failure_rate_list = Arrays.stream(stage_failure_rate)
-                .boxed()
-                .collect(Collectors.toList());
-        System.out.println(stage_failure_rate_list);
-        stage_failure_rate_list.remove(3);
-        System.out.println(stage_failure_rate_list);
-
-        Map map = new HashMap();
-        for(int i = 1; i < stage_failure_rate.length; i++){
-            map.put(i, stage_failure_rate[i]);
+        HashMap<Integer, Double> failmap = new HashMap<Integer, Double>();
+        for(int i=0; i<N;i++) {
+            failmap.put(i+1, fail[i]);
         }
-        System.out.println(map);
+        int[] answer = new int[N];
+        for(int i=0; i<N; i++) {
+            double max = -1;
+            int maxkey = 0;
+            for ( Integer key : failmap.keySet()) {
+                if(max <failmap.get(key)) {
+                    max = failmap.get(key);
+                    maxkey = key;
+                }
+            }
+            answer[i]=maxkey;
+            failmap.remove(maxkey);
+        }
+
+
 
 
 
         return answer;
+//        int[] answer = new int[n];
+//        int[] player_failed = new int[n+1];
+//        double[] stage_failure_rate = new double[n+1];
+//        int total_player = stages.length;
+//        for(int i = 0; i < stages.length; i++){
+//            if(stages[i] <= n) {
+//                player_failed[stages[i]] += 1;
+//            }else{
+//                continue;
+//            }
+//        }
+//        for(int s: player_failed){
+//            System.out.println(s);
+//        }
+//        for(int i = 1; i < stage_failure_rate.length; i++){
+//            System.out.println("player_failed[i]: " + player_failed[i]);
+//            if(player_failed[i] > 0) {
+//                stage_failure_rate[i] = (double) player_failed[i] / total_player;
+//                total_player -= player_failed[i];
+//            }else{
+//                stage_failure_rate[i] = 0;
+//            }
+//        }
+//
+//        List stage_failure_rate_list = Arrays.stream(stage_failure_rate)
+//                .boxed()
+//                .collect(Collectors.toList());
+//        System.out.println(stage_failure_rate_list);
+//        stage_failure_rate_list.remove(3);
+//        System.out.println(stage_failure_rate_list);
+//
+//        Map map = new HashMap();
+//        for(int i = 1; i < stage_failure_rate.length; i++){
+//            map.put(i, stage_failure_rate[i]);
+//        }
+//        System.out.println(map);
+//
+//
+//
+//        return answer;
 //        int player = stages.length;
 //        Arrays.sort(stages);
 //        List<Integer> list = Arrays.stream(stages).boxed().collect(Collectors.toList());
