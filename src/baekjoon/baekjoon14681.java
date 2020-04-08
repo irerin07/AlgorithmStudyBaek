6e030da2e8594bd73a6b54ee946aef7af4f4fef6
irerin07 <counterchord89@gmail.com>
2020-04-08 18:14:12 +0900

    -

--- src/baekjoon/baekjoon14681.java
+++ src/baekjoon/baekjoon14681.java
@@ -1,4 +1,17 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class baekjoon14681 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(
+                (a>0 && b>0)?1:
+                        (a>0&&b<0)?4:
+                                (a<0&&b<0)?3:
+                                        (a<0&&b>0)?2:0
+        );
+    }
 }
