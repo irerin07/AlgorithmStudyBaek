a90a72815cd12d8ad3f82e19eac467486214feac
irerin07 <counterchord89@gmail.com>
2020-04-09 19:23:30 +0900

    더하기 사이클

---
+++ src/baekjoon/baekjoon1110.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class baekjoon1110 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        int number = n;
+        int count = 0;
+
+        do {
+            number = number % 10 * 10 + (number / 10 + number % 10) % 10;
+            count++;
+        } while (n != number);
+
+        System.out.println(count);
+
+    }
+}
+
