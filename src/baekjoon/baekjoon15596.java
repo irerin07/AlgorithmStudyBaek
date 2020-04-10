e065db7d175b25ce74b4cbb444578ccc6e489e2b
irerin07 <counterchord89@gmail.com>
2020-04-10 20:47:28 +0900

    정수 N개의 합

---
+++ src/baekjoon/baekjoon15596.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+public class baekjoon15596 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        System.out.println(sum(arr));
+
+    }
+    private static long sum(int[] a){
+        long answer = 0;
+        for(int i: a){
+            answer = answer + (long)i;
+        }
+        return answer;
+    }
+}
