a3644f9c7700561ec393a2c6c26a94d6886ed2c2
irerin07 <counterchord89@gmail.com>
2020-04-16 18:32:11 +0900

    수 정렬하기

--- src/baekjoon/baekjoon10989.java
+++ src/baekjoon/baekjoon10989.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class baekjoon10989 {
     public static void main(String[] args) throws Exception {
@@ -11,15 +12,24 @@ public class baekjoon10989 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[10001];
+        int[] arr = new int[n];
         for(int i = 0; i < n; i++){
-            arr[Integer.parseInt(br.readLine())]++;
+            int index = Integer.parseInt(br.readLine());
+            arr[i] = index;
         }
-        for (int i = 1; i < 10001; i++) {
-            while (0 < arr[i]--) {
-                bw.write(i + "\n");
-            }
+        Arrays.sort(arr);
+        for(int s: arr){
+            System.out.println(s);
         }
+
+
+
+//        for (int i = 1; i < 10001; i++) {
+//            while (0 < arr[i]--) {
+//
+//                bw.write(i + "\n");
+//            }
+//        }
         br.close();
         bw.close();
     }
